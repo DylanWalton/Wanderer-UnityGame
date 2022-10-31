708cedb79cd1d2b69d8e8224f86298ad59f4f76c
DylanWalton <78657745+DylanWalton@users.noreply.github.com>
2022-10-31 15:46:29 +0100

    Update Health.cs

--- Health.cs
+++ Health.cs
@@ -2,31 +2,34 @@ using UnityEngine;
 
 public class Health : MonoBehaviour
 {
-    public float maxHealth;
-    float health;
-    
+    public float health;
+    public Animator anim;
+    public Rigidbody2D rb;
+    public BoxCollider2D clder;
+
     [Header("Damage Effect")]
     public DamageEffect damageEffect;
     public float damageEffectDuration;
     
-    public void TakeDamage(float damage)
+    public void TakeDamage(int damage)
     {
         health -= damage;
 
 
-        if (health < 0)
+        if (health <= 0 && anim != null)
         {
-            Destroy(gameObject);
+            Destroy(clder);
+            anim.SetTrigger("Death");
+            Destroy(rb);
         }
+        else if (health <= 0)
+            Destroy(gameObject);
 
         if (damageEffect != null)
         {
             StartCoroutine(damageEffect.Effect(damageEffectDuration));
         }
-    }
-    
-    void Awake()
-    {
-        health = maxHealth;
+        else if (anim != null)
+            anim.SetTrigger("Hurt");
     }
 }
