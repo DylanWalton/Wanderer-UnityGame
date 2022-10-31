5f566dea59c0a1d31b91ce4978f01481a75a8038
DylanWalton <78657745+DylanWalton@users.noreply.github.com>
2022-10-31 18:12:27 +0100

    Update PlayerHealth.cs

--- PlayerHealth.cs
+++ PlayerHealth.cs
@@ -1,19 +1,36 @@
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class PlayerHealth : MonoBehaviour
 {
-    int health;
+    float health;
     PlayerStats stats;
+
+    public Image healthBarFill;
+    public Image healthBarEffect;
+    public float effectLerpSpeed;
+    //public GameObject healthText;
+    //TextMeshPro m_HealthText;
 
     private void Awake()
     {
         stats = GetComponent<PlayerStats>();
         health = stats.maxHealth;
+        //m_HealthText = healthText.GetComponent<TextMeshPro>();
+        //m_HealthText.text = stats.maxHealth + " / " + stats.maxHealth;
     }
 
     public void TakeDamage(int damage)
     {
         health -= damage;
+
+        // Health Bar Stuff
+        healthBarFill.fillAmount = health/stats.maxHealth;
+        while (healthBarEffect.fillAmount > healthBarFill.fillAmount)
+        {
+            healthBarEffect.fillAmount = Mathf.Lerp(healthBarEffect.fillAmount, healthBarFill.fillAmount, effectLerpSpeed);
+        }
 
         if (health <= 0) {
             Die();
@@ -26,6 +43,13 @@ public class PlayerHealth : MonoBehaviour
             health = stats.maxHealth;
         else
             health += amount;
+
+        // Health Bar Stuff
+        healthBarFill.fillAmount = health / stats.maxHealth;
+        //while (healthBarEffect.fillAmount > healthBarFill.fillAmount)
+        //{
+        //    healthBarEffect.fillAmount = Mathf.Lerp(healthBarEffect.fillAmount, healthBarFill.fillAmount, effectLerpSpeed);
+        //}
     }
 
     void Die()
